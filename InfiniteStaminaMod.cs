1f3959f78f11e5722a91c91fdac382045e0c46da
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2026-05-22 11:02:13 +0200

    Add files via upload

--- InfiniteStaminaMod.cs
+++ InfiniteStaminaMod.cs
@@ -1,43 +1,63 @@
 using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api.ModOptions;
 using HarmonyLib;
-using Il2CppAssets.Scripts.Unity.UI_New.InGame;
 using Il2CppAssets.Scripts.Unity.UI_New.Legends;
-using InfiniteStamina;
 using MelonLoader;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(InfiniteStaminaMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonInfo(typeof(InfiniteStamina.InfiniteStaminaMod), InfiniteStamina.ModHelperData.Name,
+    InfiniteStamina.ModHelperData.Version, InfiniteStamina.ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace InfiniteStamina;
 
 public class InfiniteStaminaMod : BloonsTD6Mod
 {
-    public static readonly ModSettingHotkey RestoreStamina = new(KeyCode.F9);
-    public static readonly ModSettingHotkey ToggleInfiniteStamina = new(KeyCode.F10, HotkeyModifier.Shift);
-    public static readonly ModSettingBool InfiniteStaminaEnabled = new(false) { button = true };
+    public static readonly ModSettingBool InfiniteStaminaEnabled = new(false)
+    {
+        button = true,
+        description = "Eliminates all stamina costs on the Frontier map",
+    };
 
-    [HarmonyPatch(typeof(FrontierMap), nameof(FrontierMap.Update))]
-    internal static class FrontierMap_Update
+    public static readonly ModSettingBool InstantRegenEnabled = new(false)
+    {
+        button = true,
+        description = "Massively increases passive stamina regeneration speed",
+    };
+
+    public static readonly ModSettingHotkey RestoreStamina = new(KeyCode.F9)
+    {
+        description = "Instantly restores all party stamina and revives knocked monkeys",
+    };
+
+    public override void OnUpdate()
+    {
+        if (!RestoreStamina.JustPressed()) return;
+
+        var manager = FrontierLegendsManager.instance;
+        if (manager is null) return;
+
+        manager.ClearKnockedMonkeys();
+        manager.HealPartyToPercent(1f);
+        manager.AddHomesteadStamina(9999f, true);
+    }
+
+    [HarmonyPatch(typeof(FrontierLegendsManager), nameof(FrontierLegendsManager.UpdateModifiers))]
+    internal static class FrontierLegendsManager_UpdateModifiers
     {
         [HarmonyPostfix]
-        internal static void Postfix()
+        internal static void Postfix(FrontierLegendsManager __instance)
         {
-            var manager = FrontierLegendsManager.instance;
-            if (manager is null) return;
+            if (!InfiniteStaminaEnabled && !InstantRegenEnabled) return;
 
-            if (RestoreStamina.JustPressed())
-            {
-                manager.HealPartyToPercent(1f);
-                manager.AddHomesteadStamina(9999f, 1f);
-            }
-
-            if (ToggleInfiniteStamina.JustPressed())
-                InfiniteStaminaEnabled.SetValue(!InfiniteStaminaEnabled);
+            var modifiers = __instance.Modifiers;
+            if (modifiers is null) return;
 
             if (InfiniteStaminaEnabled)
-                manager.Modifiers.staminaCost = 0f;
+                modifiers.staminaCost = 0f;
+
+            if (InstantRegenEnabled)
+                modifiers.passiveStaminaRegenMultiplier = 9999f;
         }
     }
 }
--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,13 @@ namespace InfiniteStamina;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "52.2";
-    public const string Version = "1.1.0";
+    public const string WorksOnVersion = "54.3";
+    public const string Version = "2.0.0";
     public const string Name = "Infinite Stamina";
-    public const string Description = "Provides hotkeys to restore and toggle infinite stamina in Frontier Legends.";
+    public const string Description = "Toggle infinite stamina (zero cost) and instant regeneration for Frontier Legends. Press F9 to fully restore party stamina and revive knocked monkeys.";
     public const string RepoOwner = "Myself-Bloons";
     public const string RepoName = "InfiniteStamina";
     public const string Author = "Myself";
     public const string DllName = "InfiniteStamina.dll";
+    public const string Icon = "Icon.png";
 }
